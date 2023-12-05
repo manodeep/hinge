95655c089d6c555f015006a833356d5d490962fd
Manodeep Sinha <manodeep@gmail.com>
2023-12-04 18:42:26 -0600

    Fixed typo in Subfind halo filename

--- orphanfixer/main.c
+++ orphanfixer/main.c
@@ -1,12 +1,12 @@
 /*
 
-Log started: July 2009: MS. 
+Log started: July 2009: MS.
 
 07/01/2009:   started writing the codes. Complete with a Makefile.
 
 07/19/2009:   loadgroups now written and compiles. Needs to be cross-matched
-              with known-good IDL routines to make sure that the data is read 
-			  in correctly. 
+              with known-good IDL routines to make sure that the data is read
+			  in correctly.
 
 08/24/2009:   No more parent checking. Just read in the parents_xxx.txt and
               assign them. Added 3 categories for mergers:
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
   /* char str_line[MAXLEN]; */
   /* int line=0; */
   time_t t_codestart,t_codeend,t_sectionstart,t_sectionend,t_bigsectionstart;
-  
+
   t_codestart = time(NULL);
 
   /* Check compilation options */
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
 
 #ifdef BIGSIM
   if(sizeof(size_t) != 8)
-    { 
+    {
       fprintf(stderr,"Error: Code needs to be compiled in 64 bit mode \n");
       fprintf(stderr,"Please add -m64 to the options in the Makefile ..(or find a 64 bit compiler)\n");
       fprintf(stderr,"Exiting..\n");
@@ -75,13 +75,13 @@ int main(int argc, char **argv)
 
   // Check command line for the parameter file name.
   if (argc !=2)
-    { 
+    {
       fprintf(stderr,"Usage: %s <parameterfile>\n", argv[0]);
 	  fprintf(stderr,"\n\n\tCode was compiled with \n\n");
 	  print_makefile_options();
       exit(EXIT_FAILURE);
     }
-  
+
   my_snprintf(outfname,MAXLEN,"%s",argv[1]);//parameter file
 
   //read in the parameter file
@@ -98,34 +98,12 @@ int main(int argc, char **argv)
 
   //the declarations that depend on MAX_SNAPSHOT_NUM
   //If MAX_SNAPSHOT_NUM is too large, consider mallocing these
-  NUM_SNAPSHOTS = PARAMS.MAX_SNAPSHOT_NUM+1;    
+  NUM_SNAPSHOTS = PARAMS.MAX_SNAPSHOT_NUM+1;
   int64 Ngroups[NUM_SNAPSHOTS];//there are groups corresponding to MAX_SNAPSHOT_NUM
   struct parent_data *  allparents[PARAMS.MAX_SNAPSHOT_NUM];//parents_??? files only go up to MAX_SNAPSHOT_NUM-1
   struct node_data *    tree[NUM_SNAPSHOTS];//there are groups in MAX_SNAPSHOT_NUM
 
-  
-  //read in boxsize and massarr from Gadget header
-  /* my_snprintf(snapshotname,MAXLEN,"%s/%s_%03d",PARAMS.SNAPSHOT_DIR,PARAMS.SNAPSHOT_BASE,PARAMS.MAX_SNAPSHOT_NUM); */
-#ifndef SUSSING_TREES
-  my_snprintf(snapshotname,MAXLEN,"%s/%s_%03d",PARAMS.SNAPSHOT_DIR,PARAMS.SNAPSHOT_BASE,PARAMS.MAX_SNAPSHOT_NUM);//use this line if only one snapshot is available.
-#else
-  my_snprintf(snapshotname,MAXLEN,"%s/snapdir_%03d/%s_%03d",PARAMS.SNAPSHOT_DIR,PARAMS.MAX_SNAPSHOT_NUM,PARAMS.SNAPSHOT_BASE,PARAMS.MAX_SNAPSHOT_NUM);//use this line if only one snapshot is available.
-#endif
-
-#ifdef SUBFIND	
-  header = get_gadget_header(snapshotname);
-  NUMPART = get_Numpart(&header);
-  for(int i=0;i<6;i++) {
-    //needed to make Rvir calculation
-    if(header.npart[i] > 0 && header.mass[i] <= 0.0) {
-      fprintf(stderr,"ERROR: Gadget snapshot has individual masses. This code can not handle that yet\n");//loadgroups with fof_only will not be able to handle this.
-      exit(EXIT_FAILURE);
-    }
-    PARAMS.MASSARR[i] = header.mass[i];
-  }
-#endif	
-
-  // output the parameter file 
+  // output the parameter file
   my_snprintf(outfname,MAXLEN,"%s/orphanfixer.params",PARAMS.OUTPUT_DIR);
   fprintf(stderr,"output parameter file to `%s'...",outfname);
   output_params(outfname,&PARAMS);
@@ -158,9 +136,9 @@ int main(int argc, char **argv)
 		}
 		fclose(fd);
 	}
-	
-	
-  
+
+
+
   set_cosmology(&COSMO);
   PARAMS.COSMO = &COSMO;
   set_simulation_params(&PARAMS);
@@ -174,14 +152,14 @@ int main(int argc, char **argv)
     t_bigsectionstart = time(NULL);
     fprintf(stderr,"\n\n Now working on snapshot# %4d \n",isnapshot);
 
-    /* read in Ngroups.  */    
+    /* read in Ngroups.  */
 #ifdef SUBFIND
-		my_snprintf(outfname, MAXLEN,"%s/groups_%03d.fofcat", PARAMS.GROUP_DIR,isnapshot);
+		my_snprintf(outfname, MAXLEN,"%s/groups_%03d.subcat", PARAMS.GROUP_DIR,isnapshot);
 		Ngroups0 = returnNhalo(outfname);
 #endif
 
 
-		
+
 #ifdef SUSSING_TREES
 #define RETURN_ONLY_FOFS 0
 		/* my_snprintf(outfname,MAXLEN,"%s/%s_%03d.z%5.3f.AHF_halos", PARAMS.GROUP_DIR, PARAMS.GROUP_BASE,isnapshot,REDSHIFT[isnapshot]); */
@@ -199,7 +177,7 @@ int main(int argc, char **argv)
       if(isnapshot < PARAMS.MAX_SNAPSHOT_NUM) {
 				parent = my_malloc(sizeof(*parent),Ngroups0);
 				my_snprintf(outfname, MAXLEN,"%s/parents_%03d.txt",PARAMS.OUTPUT_DIR,isnapshot);
-				
+
 				fprintf(stderr,"Reading in parents from `%s' Ngroups0 = %"STR_FMT"\n",outfname,Ngroups0);
 				t_sectionstart = time(NULL);
 				parent  = loadparents(outfname,parent,Ngroups0);
@@ -208,24 +186,24 @@ int main(int argc, char **argv)
 				print_time(t_sectionstart,t_sectionend,"loadparents");
 				allparents[isnapshot] = parent;
       }
-      
+
       group0=allocate_group(Ngroups0);//allocate and initialize
       fprintf(stderr,"loading group for snapshot # %d with %"STR_FMT" halos ",isnapshot,Ngroups0);
-      
-      t_sectionstart = time(NULL);	  
+
+      t_sectionstart = time(NULL);
       loadgroups(isnapshot,group0);
       fprintf(stderr," done ...\n\n");
       t_sectionend = time(NULL);
       print_time(t_sectionstart,t_sectionend,"loadgroups");
       Ngroups[isnapshot] = Ngroups0;
-      
-      t_sectionstart = time(NULL);	  
+
+      t_sectionstart = time(NULL);
       my_snprintf(outfname,MAXLEN,"%s/subhalolevel_%03d.txt",PARAMS.OUTPUT_DIR,isnapshot);
       readsubhalo_hierarchy_levels(outfname,group0);
       fprintf(stderr," done ...\n\n");
       t_sectionend = time(NULL);
       print_time(t_sectionstart,t_sectionend,"subhalo hierarchy levels");
-      
+
       /*Read in the actual snapshot */
 #if (defined(GET_GROUPVEL) == 0 && defined(GET_MEANVEL) == 0)
       my_snprintf(snapshotname, MAXLEN,"%s/%s_%03d",PARAMS.SNAPSHOT_DIR,PARAMS.SNAPSHOT_BASE,isnapshot);
@@ -233,32 +211,32 @@ int main(int argc, char **argv)
       P = loadsnapshot(snapshotname,&header);
       t_sectionend = time(NULL);
       print_time(t_sectionstart,t_sectionend,"loadsnapshot");
-      
+
       t_sectionstart = time(NULL);
       assign_vxcm(group0,Ngroups0,P,REDSHIFT[isnapshot]);
       t_sectionend = time(NULL);
       print_time(t_sectionstart,t_sectionend,"assign meanvel from snapshot");
       my_free((void **) &P);
 #endif
-      
+
       /* assign the groups for their snapshot number to the correct locations*/
       node = my_malloc(sizeof(*node),Ngroups0);
       t_sectionstart = time(NULL);
       assign_node(group0,Ngroups0,parent,node,isnapshot);
       t_sectionend = time(NULL);
       print_time(t_sectionstart,t_sectionend,"assign_node");
-      
+
       /* Now store the node data in the tree */
       tree[isnapshot] = node;
       free_group(group0,Ngroups0);
     }
-    
+
     my_snprintf(outfname, MAXLEN,"%s %d ","Reading groups and assigning to node for i = ",isnapshot);/*use outfname as temporary message variable. Gets reset..*/
     t_sectionend = time(NULL);
     print_time(t_bigsectionstart,t_sectionend,outfname);
   }
-  
-  
+
+
   /* All the groups have now been assigned in the node/tree structure. Make the parent/child pointer associations */
   t_sectionstart = time(NULL);
   maketree(allparents,Ngroups,tree);
@@ -270,7 +248,7 @@ int main(int argc, char **argv)
   assign_haloid(tree,Ngroups);
   t_sectionend = time(NULL);
   print_time(t_sectionstart,t_sectionend,"assign_haloid");
-  
+
 #ifndef FOF_ONLY
   if(PARAMS.LOAD_FOUND_PROGENITORS==1) {
     my_snprintf(outfname,MAXLEN,"%s/found_progenitors.txt",PARAMS.OUTPUT_DIR);
@@ -288,7 +266,7 @@ int main(int argc, char **argv)
     t_sectionend = time(NULL);
     print_time(t_sectionstart,t_sectionend,"fill progenitors");
   }
-  
+
 
 /*   t_sectionstart = time(NULL); */
 /*   output_missing_halo_centres(tree,Ngroups); */
@@ -305,7 +283,7 @@ int main(int argc, char **argv)
 /*   print_time(t_sectionstart,t_sectionend,"For SUSSING Merger Tree comparison project"); */
 /* #endif   */
 
-  
+
   /* free up the memory */
   for(int isnapshot=PARAMS.MIN_SNAPSHOT_NUM;isnapshot<=PARAMS.MAX_SNAPSHOT_NUM;isnapshot++) {
     if (Ngroups[isnapshot] > 0 ) {
@@ -315,17 +293,17 @@ int main(int argc, char **argv)
       free(tree[isnapshot]);
     }
   }
-  
+
   free(REDSHIFT);
   free(PARAMS.Age);
   fprintf(stderr,"\n\n Done...\n\n");
-  
+
   t_codeend = time(NULL);
   print_time(t_codestart,t_codeend,"Entire code");
-  
+
   exit(EXIT_SUCCESS);
-  
-}	
+
+}
 
 
 
@@ -344,7 +322,7 @@ void assign_vxcm(struct group_data *group,int64 Ngroups,struct particle_data *P,
     sumvy=0.0;
     sumvz=0.0;
     summ=0.0;
-	  
+
 #ifdef FOF_ONLY
     group[i].Mtot=0.0;
 #endif
@@ -359,17 +337,17 @@ void assign_vxcm(struct group_data *group,int64 Ngroups,struct particle_data *P,
       sumx += (periodic(P[index].Pos[0]-group[i].xcen)*mass); /* computing the centre of mass -> needs to account for box wrapping. */
       sumy += (periodic(P[index].Pos[1]-group[i].ycen)*mass);
       sumz += (periodic(P[index].Pos[2]-group[i].zcen)*mass);
-#else 
+#else
       sumx += (periodic(P[index].Pos[0]-group[i].xcen)*mass); /* Make sure that group[i].xcen is initiliased by loadgroups under the FOF_ONLY flag. */
       sumy += (periodic(P[index].Pos[1]-group[i].ycen)*mass);
       sumz += (periodic(P[index].Pos[2]-group[i].zcen)*mass);
-#endif 
-      
+#endif
+
 #ifdef FOF_ONLY
       group[i].Mtot += mass;
 #endif
     }
-	  
+
 #ifdef FOF_ONLY
     group[i].xcen = periodic_wrap(sumx/summ);
     group[i].ycen = periodic_wrap(sumy/summ);
@@ -379,23 +357,23 @@ void assign_vxcm(struct group_data *group,int64 Ngroups,struct particle_data *P,
     group[i].Yoffset = group[i].ycen - periodic_wrap(sumy/summ);
     group[i].Zoffset = group[i].zcen - periodic_wrap(sumz/summ);
 #endif
-    
+
     group[i].vxcen = sumvx/summ;
     group[i].vycen = sumvy/summ;
     group[i].vzcen = sumvz/summ;
-    
+
     group[i].vxcen *= sqrt(get_scalefactor(redshift));
     group[i].vycen *= sqrt(get_scalefactor(redshift));
     group[i].vzcen *= sqrt(get_scalefactor(redshift));
-    
+
   }
-  
+
 }
 
 
 void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *parent,struct node_data *node,int isnapshot)
 {
-  
+
 #ifdef GET_MEANVEL
   FILE *fp=NULL;
   int64 tmp_grpnum=0;
@@ -409,14 +387,14 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
       if(str_line[0] !=comment)
 	break;
     }
-  
+
 #endif
 
   struct node_data *FOF_Parent=NULL;
   float scale_factor = 1.0/(1.0+REDSHIFT[isnapshot]);
   float root_a = sqrt(scale_factor);
   double boundfofmtot=0.0;
-  
+
   for(int64 igroup = 0;igroup<Ngroups0;igroup++) {
     if(FOF_Parent!=NULL && group0[igroup].isFof==1) {
       /* this will assign the sum of all the subhalo masses as the bound fof mtot. however, this
@@ -424,7 +402,7 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
 	 need to check for that at the end of the loop */
       FOF_Parent->BoundFofMtot = boundfofmtot;
     }
-      
+
     if(group0[igroup].isFof == 1) {
       FOF_Parent = &node[igroup];
       boundfofmtot = group0[igroup].Mtot;
@@ -432,13 +410,13 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
       boundfofmtot += group0[igroup].Mtot;
       node[igroup].BoundFofMtot = group0[igroup].Mtot; /* Not really proper -- subhalos have their 'normal' mass as the bound fof mass */
     }
-      
+
     node[igroup].isFof = group0[igroup].isFof;
     /* 	  node[igroup].Nsub     = group0[igroup].Nsub; */
     node[igroup].haloid = -1;
 #ifdef SUSSING_TREES
     node[igroup].SUSSING_haloID = group0[igroup].haloID;
-#endif    
+#endif
 
     node[igroup].nodeloc = igroup;
     node[igroup].z = REDSHIFT[isnapshot];
@@ -446,31 +424,31 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
     node[igroup].xcen = group0[igroup].xcen;
     node[igroup].ycen = group0[igroup].ycen;
     node[igroup].zcen = group0[igroup].zcen;
-    
+
 #ifdef GET_GROUPVEL
     node[igroup].vxcen = group0[igroup].vxcen*root_a;
     node[igroup].vycen = group0[igroup].vycen*root_a;
     node[igroup].vzcen = group0[igroup].vzcen*root_a;
-#else 
+#else
 #ifndef GET_MEANVEL
     node[igroup].vxcen = group0[igroup].vxcen;/* group vels have been loaded in from snapshots. Copy them to the nodes. */
     node[igroup].vycen = group0[igroup].vycen;
     node[igroup].vzcen = group0[igroup].vzcen;
 #endif
 #endif
-    
+
     node[igroup].Mtot = group0[igroup].Mtot;
     node[igroup].ContainerId     = group0[igroup].ContainerIndex;
     node[igroup].ParentLevel     = group0[igroup].ParentLevel;
     node[igroup].Nsub            = group0[igroup].Nsub;
     node[igroup].ContainerHalo   = &(node[node[igroup].ContainerId]);
-    
+
     if((node[igroup].ContainerHalo)->Nsub == 0) {
       fprintf(stderr,"This should not have happened. The container claims to have no Nsubs. snapshot = %hd igroup = %"STR_FMT" Container id = %"STR_FMT" ..exiting\n",
 	      isnapshot,igroup,node[igroup].ContainerId);
       exit(EXIT_FAILURE);
     }
-    
+
     if(isnapshot < PARAMS.MAX_SNAPSHOT_NUM) {
       if (parent[igroup].parentid >=0 && parent[igroup].parentsnapshot <= PARAMS.MAX_SNAPSHOT_NUM) {
 	/* 			  fprintf(stderr,"isnapshot = %d igroup = %d parent snapshot = %d\n",isnapshot,igroup,parent[igroup].parentsnapshot); */
@@ -483,7 +461,7 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
       node[igroup].ParentZ         = -1.0;
       node[igroup].ParentSnapshot  = -1;
     }
-    
+
     node[igroup].FofHalo  = FOF_Parent;
     node[igroup].Sibling  = NULL;
     node[igroup].Parent   = NULL;
@@ -496,7 +474,7 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
       fprintf(stderr,"expected groupnum = %"STR_FMT"  got groupnum = %"STR_FMT" ..exiting\n",igroup,tmp_grpnum);
       exit(EXIT_FAILURE);
     }
-    
+
     if(fgets(str_line, MAXLINESIZE,fp)==NULL  && igroup != (Ngroups0-1)) {
       fprintf(stderr,"Error (eof): expected more lines to read in from group velocities file `%s' \n",fname);
       fprintf(stderr,"currently at igroup = %"STR_FMT" ..exiting\n",igroup);
@@ -504,12 +482,12 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
     }
 
       /*
-		MS: 5th Aug, 2010. Took out the Gadget sqrt(a) dependence of v 
+		MS: 5th Aug, 2010. Took out the Gadget sqrt(a) dependence of v
       */
 
     for(int k=0;k<3;k++)
       node[igroup].meanvel[k] *= root_a;
-    
+
 #else
 #ifdef GET_GROUPVEL
       node[igroup].meanvel[0] = node[igroup].vxcen;
@@ -526,7 +504,7 @@ void assign_node(struct group_data *group0,int64 Ngroups0,struct parent_data *pa
 
 
 
-      
+
     }
 #ifdef GET_MEANVEL
   fclose(fp);
@@ -544,16 +522,16 @@ void readsubhalo_hierarchy_levels(const char *fname,struct group_data *group0)
   short dummy1;
   fp = my_fopen(fname,"r");
   i=0;
-  while(1) { 
+  while(1) {
     if(fgets(str_line, MAXLINESIZE,fp)!=NULL) {
       if(str_line[0] !=comment) {
 	/*                    sscanf(str_line,"%*"STR_FMT" %"STR_FMT" %hd   %"STR_FMT"  %"STR_FMT"     %hd",  */
 	sscanf(str_line,"%*d   %"STR_FMT" %hd   %"STR_FMT"  %"STR_FMT"     %hd",
 	       &dummy,&(group0[i].ParentLevel),&(group0[i].ContainerIndex),&(group0[i].Nsub),&dummy1); /*discarding the first field, Fofid */
-	//The first field in the hierarchy file is of type int64. But since I am suppressing 
+	//The first field in the hierarchy file is of type int64. But since I am suppressing
 	//that field -> I can as well use the %d type [and this way the compiler warning goes away.
-	
-	
+
+
 	if(dummy != i) {
 	  fprintf(stderr,"Error: While reading in file `%s' for subhalo hierarchy level \n ",fname);
 	  fprintf(stderr,"expected igroup = %"STR_FMT" instead got %"STR_FMT" ..exiting \n\n",i,dummy);
@@ -577,14 +555,14 @@ void output_data_for_SUSSING_TREES(struct node_data *tree[],int64 *Ngroups)
   char outfname[MAXLEN];
   int64_t SumTotal_AllHalos=0;
   int64_t Nbad=0;
-  
+
   char date[9];
   time_t t = time(0);
   struct tm *tm;
   tm = gmtime(&t);
   strftime(date, sizeof(date), "%Y%m%d", tm);
 
-  for(int isnapshot=PARAMS.MAX_SNAPSHOT_NUM;isnapshot>=PARAMS.MIN_SNAPSHOT_NUM;isnapshot--) {  
+  for(int isnapshot=PARAMS.MAX_SNAPSHOT_NUM;isnapshot>=PARAMS.MIN_SNAPSHOT_NUM;isnapshot--) {
     SumTotal_AllHalos += Ngroups[isnapshot];
   }
 
@@ -593,7 +571,7 @@ void output_data_for_SUSSING_TREES(struct node_data *tree[],int64 *Ngroups)
   fprintf(fp,"1\n");
   fprintf(fp,"Halo Interaction Network and Galaxy Evolution - settings used from previous experience with SUBFIND. v1.0, %s \n",date);
   fprintf(fp,"%"PRId64"\n",SumTotal_AllHalos);
-  
+
   for(int isnapshot=PARAMS.MAX_SNAPSHOT_NUM;isnapshot>=PARAMS.MIN_SNAPSHOT_NUM;isnapshot--) {
     BaseNode = tree[isnapshot];
     for(int64 igroup=0;igroup<Ngroups[isnapshot];igroup++) {
@@ -601,7 +579,7 @@ void output_data_for_SUSSING_TREES(struct node_data *tree[],int64 *Ngroups)
       fprintf(fp,"%ld  \t %"STR_FMT" \n",thisnode->SUSSING_haloID,thisnode->Nchild);
       if(thisnode->isFof==0 && thisnode->BigChild==NULL && thisnode->FofHalo->BigChild != NULL)
 	Nbad++;
-      
+
       thisnode = thisnode->BigChild;
       while(thisnode != NULL) {
 	fprintf(fp,"%ld \n",thisnode->SUSSING_haloID);
@@ -609,19 +587,19 @@ void output_data_for_SUSSING_TREES(struct node_data *tree[],int64 *Ngroups)
       }
     }
   }
-  
-  
+
+
   fprintf(fp,"END\n");
   fclose(fp);
   fprintf(stderr,"Nbad = %"PRId64"\n",Nbad);
 }
 #endif
-    
+
 
 
 void print_makefile_options(void)
 {
-  
+
 
 #ifdef FOF_ONLY
   fprintf(stderr,"The code is going to read in FOF groups only\n");
@@ -671,9 +649,9 @@ void print_makefile_options(void)
 #ifdef SUSSING_TREES
   fprintf(stderr,"The code will assume data for the SUSSING Mergertree Comparison Project\n");
 #endif
-  
+
 #ifdef ASCII_DATA
   fprintf(stderr,"The code will read in ASCII input data (only valid with -DSUSSING_TREES; ignored otherwise) \n");
 #endif
-  
+
 }
