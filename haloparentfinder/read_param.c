8dc8b2316a67d6d2cca2064bd2e78952066386dd
Manodeep Sinha <manodeep@gmail.com>
2024-06-14 23:18:00 +1000

    used xassert with print

--- haloparentfinder/read_param.c
+++ haloparentfinder/read_param.c
@@ -9,32 +9,32 @@ void haloparentfinder_fill_params(struct params_data *params, const int maxtags,
     my_snprintf(tag[nt], MAXLEN, "MAX_INCR");
     addr[nt] = &(params->MAX_INCR);
     id[nt++] = INT;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     my_snprintf(tag[nt], MAXLEN, "MAX_RANK_LOC");
     addr[nt] = &(params->MAX_RANK_LOC);
     id[nt++] = INT64;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     my_snprintf(tag[nt], MAXLEN, "MIN_FCOMMON_FINDPROGENITOR_THRESH");
     addr[nt] = &(params->MIN_FCOMMON_FINDPROGENITOR_THRESH);
     id[nt++] = DOUBLE;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     my_snprintf(tag[nt], MAXLEN, "MIN_NUMPART_IN_FINDPROGENITOR_HALO");
     addr[nt] = &(params->MIN_NUMPART_IN_FINDPROGENITOR_HALO);
     id[nt++] = INT64;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     my_snprintf(tag[nt], MAXLEN, "MIN_FCOMMON_SWITCHFOF_THRESH");
     addr[nt] = &(params->MIN_FCOMMON_SWITCHFOF_THRESH);
     id[nt++] = DOUBLE;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     my_snprintf(tag[nt], MAXLEN, "MIN_NUMPART_IN_SWITCHFOF_HALO");
     addr[nt] = &(params->MIN_NUMPART_IN_SWITCHFOF_HALO);
     id[nt++] = INT64;
-    assert(nt < maxtags);
+    XASSERT(nt < maxtags, "Error: parameter index =%d must be less than max number of parameters=%d\n", nt, maxtags);
 
     *nt_out = nt;
 }
