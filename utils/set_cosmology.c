c583510e38127635b4a8fc9fdadf811a3814d57d
clang-format Action <manodeep@gmail.com>
2024-06-16 11:59:13 +0000

    Committing clang-format changes

--- utils/set_cosmology.c
+++ utils/set_cosmology.c
@@ -261,7 +261,8 @@ void getrvir_from_overdensity(struct group_data *group, int NBINS, const double 
     }
 
     XASSERT(rmin > 0, "Error: Min. radius = %f must be non-zero\n", rmin);
-    XASSERT(rmax > rmin, "Error: Max. radius = %f must be greater than minimum radius = %f. Npart = %"STR_FMT"\n", rmax, rmin, Npart);
+    XASSERT(rmax > rmin, "Error: Max. radius = %f must be greater than minimum radius = %f. Npart = %" STR_FMT "\n",
+            rmax, rmin, Npart);
     XASSERT(nbins > 0, "Number of bins =%d must be non-zero", nbins);
     rbinsize = (log10(rmax) - log10(rmin)) / nbins;
     /*   for(i=0;i<nbins;i++) */
