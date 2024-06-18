0a20834b8bfe6301d4c2c903504fbf1e61d527bc
Manodeep Sinha <manodeep@gmail.com>
2024-06-19 09:20:10 +1000

    Set the particle id to be negative of itself (to preserve the id info but also skip)

--- utils/utils.c
+++ utils/utils.c
@@ -539,7 +539,8 @@ void remove_particle_from_group(const int64 group1, const int64 group2, const in
     // Keep the particle in the halo with the smaller distance from the center
     *group_to_remove = dist_from_cen1 < dist_from_cen2 ? group2 : group1;
     *part_to_remove = dist_from_cen1 < dist_from_cen2 ? part2 : part1;
-    g[*group_to_remove].id[*part_to_remove] = -1;
+    const id64 id = g[*group_to_remove].id[*part_to_remove];
+    g[*group_to_remove].id[*part_to_remove] = id < 0 ? id:-id; //Just make the sign negative
     return;
 }
 
