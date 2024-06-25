3cf85a074b0288d526f6e175d2756f3f4b7a892b
Manodeep Sinha <manodeep@gmail.com>
2024-06-26 09:53:01 +1000

    Cleaned up the code for reading previously generated orphan-fixes

--- orphanfixer/fillprogenitors.c
+++ orphanfixer/fillprogenitors.c
@@ -34,7 +34,6 @@ int compare_id64(const void *a, const void *b);
 
 short load_found_progenitors(struct node_data *tree[], int64 *Ngroups, const char *fname, int64 *startgroupnum)
 {
-    FILE *fp = NULL;
     char comment = '#';
     char str_line[MAXLINESIZE];
     int64 nlines = 0;
@@ -42,105 +41,107 @@ short load_found_progenitors(struct node_data *tree[], int64 *Ngroups, const cha
     int64 groupnum = -1, groupid, groupN, prevnum;
     struct node_data *thisnode = NULL, *BaseNode = NULL, *prevnode = NULL, *tmp_node = NULL;
 
-    fp = my_fopen(fname, "rt");
-    while (1)
+    FILE *fp = my_fopen(fname, "rt");
+    while (fgets(str_line, MAXLINESIZE, fp) != NULL)
     {
-        if (fgets(str_line, MAXLINESIZE, fp) != NULL)
+        if (str_line[0] == comment)
+            continue;
+
+        sscanf(str_line, "%hd  %" STR_FMT "  %" STR_FMT "  %" STR_FMT "  %hd  %" STR_FMT "  \n", &snapshot,
+                &groupnum, &groupid, &groupN, &prevsnap, &prevnum);
+
+        if (snapshot < PARAMS.MIN_SNAPSHOT_NUM || snapshot > PARAMS.MAX_SNAPSHOT_NUM)
         {
-            if (str_line[0] != comment)
-            {
-                sscanf(str_line, "%hd  %" STR_FMT "  %" STR_FMT "  %" STR_FMT "  %hd  %" STR_FMT "  \n", &snapshot,
-                       &groupnum, &groupid, &groupN, &prevsnap, &prevnum);
-
-                if (snapshot < PARAMS.MIN_SNAPSHOT_NUM || snapshot > PARAMS.MAX_SNAPSHOT_NUM)
-                {
-                    fprintf(stderr,
-                            "ERROR: I have snapshot = %hd  but max snapshot = %d and min "
-                            "snapshot = %d\n",
-                            snapshot, PARAMS.MAX_SNAPSHOT_NUM, PARAMS.MIN_SNAPSHOT_NUM);
-                    fprintf(stderr, "exiting..\n");
-                    exit(EXIT_FAILURE);
-                }
-
-                if (prevsnap < PARAMS.MIN_SNAPSHOT_NUM || prevsnap > PARAMS.MAX_SNAPSHOT_NUM)
-                {
-                    fprintf(stderr,
-                            "ERROR: I have prevsnap = %hd  but max snapshot = %d and min "
-                            "snapshot = %d\n",
-                            prevsnap, PARAMS.MAX_SNAPSHOT_NUM, PARAMS.MIN_SNAPSHOT_NUM);
-                    fprintf(stderr, "exiting..\n");
-                    exit(EXIT_FAILURE);
-                }
-
-                if (groupnum > Ngroups[snapshot] || prevnum > Ngroups[prevsnap])
-                {
-                    fprintf(stderr, "Inside load_found_progenitors: This should not have "
-                                    "happened\n ");
-                    fprintf(stderr, "snapshot = %hd   groupnum = %" STR_FMT " prevsnap = %hd prevnum = %" STR_FMT " \n",
-                            snapshot, groupnum, prevsnap, prevnum);
-                    fprintf(stderr, "Ngroups[%hd] = %" STR_FMT " Ngroups[%hd] = %" STR_FMT " \n", snapshot,
-                            Ngroups[snapshot], prevsnap, Ngroups[prevsnap]);
-                    fprintf(stderr, "exiting \n\n");
-                    exit(EXIT_FAILURE);
-                }
-
-                /* necessary for partial found_progenitors.txt */
-                if (snapshot < min_snapshot)
-                    min_snapshot = snapshot;
-
-                /* now switch prevnode[prevnum] to point to  currnode[groupnum] */
-                BaseNode = tree[snapshot];
-                thisnode = &BaseNode[groupnum];
-
-                BaseNode = tree[prevsnap];
-                prevnode = &BaseNode[prevnum];
-
-                if (prevnode->Parent != NULL)
-                {
-                    tmp_node = prevnode->Parent->BigChild;
-
-                    // BigChild will not be switched -- so prevnode
-                    // has to be at least the Sibling of BigChild
-                    while (tmp_node->Sibling != prevnode)
-                        tmp_node = tmp_node->Sibling;
-
-                    tmp_node->Sibling = prevnode->Sibling;
-                    prevnode->Parent->Nchild--;
-                }
-
-                prevnode->Sibling = NULL;
-                prevnode->Parent = thisnode;
-                prevnode->ParentSnapshot = thisnode->snapshot;
-                prevnode->ParentID = thisnode->nodeloc;
-                prevnode->ParentZ = thisnode->z;
-                thisnode->BigChild = prevnode;
-                thisnode->Nchild = 1;
-                /* 			  thisnode->FormationRedshift =
-                 * prevnode->FormationRedshift; */
-                /* Need to fix the haloids -- since it could be a partial
-                   found_progenitors.txt so the behaviour of a complete fillprogenitor
-                   run is mimicked.
-                */
-                tmp_node = prevnode;
-                while (tmp_node != NULL)
-                {
-                    tmp_node->haloid = thisnode->haloid;
-                    tmp_node = tmp_node->BigChild;
-                }
-
-                nlines++;
-            }
+            fprintf(stderr,
+                    "ERROR: I have snapshot = %hd  but max snapshot = %d and min "
+                    "snapshot = %d\n",
+                    snapshot, PARAMS.MAX_SNAPSHOT_NUM, PARAMS.MIN_SNAPSHOT_NUM);
+            fprintf(stderr, "exiting..\n");
+            exit(EXIT_FAILURE);
         }
-        else
+
+        if (prevsnap < PARAMS.MIN_SNAPSHOT_NUM || prevsnap > PARAMS.MAX_SNAPSHOT_NUM)
         {
-            break;
+            fprintf(stderr,
+                    "ERROR: I have prevsnap = %hd  but max snapshot = %d and min "
+                    "snapshot = %d\n",
+                    prevsnap, PARAMS.MAX_SNAPSHOT_NUM, PARAMS.MIN_SNAPSHOT_NUM);
+            fprintf(stderr, "exiting..\n");
+            exit(EXIT_FAILURE);
         }
+
+        if (groupnum > Ngroups[snapshot] || prevnum > Ngroups[prevsnap])
+        {
+            fprintf(stderr, "Inside load_found_progenitors: This should not have "
+                            "happened\n ");
+            fprintf(stderr, "snapshot = %hd   groupnum = %" STR_FMT " prevsnap = %hd prevnum = %" STR_FMT " \n",
+                    snapshot, groupnum, prevsnap, prevnum);
+            fprintf(stderr, "Ngroups[%hd] = %" STR_FMT " Ngroups[%hd] = %" STR_FMT " \n", snapshot,
+                    Ngroups[snapshot], prevsnap, Ngroups[prevsnap]);
+            fprintf(stderr, "exiting \n\n");
+            exit(EXIT_FAILURE);
+        }
+
+        /* necessary for partial found_progenitors.txt */
+        if (snapshot < min_snapshot)
+            min_snapshot = snapshot;
+
+        /* now switch prevnode[prevnum] to point to  currnode[groupnum] */
+        BaseNode = tree[snapshot];
+        thisnode = &BaseNode[groupnum];
+
+        BaseNode = tree[prevsnap];
+        prevnode = &BaseNode[prevnum];
+
+        if (prevnode->Parent != NULL)
+        {
+            tmp_node = prevnode->Parent->BigChild;
+
+            // BigChild will not be switched -- so prevnode
+            // has to be at least the Sibling of BigChild
+            while (tmp_node->Sibling != prevnode)
+                tmp_node = tmp_node->Sibling;
+
+            tmp_node->Sibling = prevnode->Sibling;
+            prevnode->Parent->Nchild--;
+        }
+
+        prevnode->Sibling = NULL;
+        prevnode->Parent = thisnode;
+        prevnode->ParentSnapshot = thisnode->snapshot;
+        prevnode->ParentID = thisnode->nodeloc;
+        prevnode->ParentZ = thisnode->z;
+        thisnode->BigChild = prevnode;
+        thisnode->Nchild = 1;
+        /* 			  thisnode->FormationRedshift =
+            * prevnode->FormationRedshift; */
+        /* Need to fix the haloids -- since it could be a partial
+            found_progenitors.txt so the behaviour of a complete fillprogenitor
+            run is mimicked.
+        */
+        tmp_node = prevnode;
+        while (tmp_node != NULL)
+        {
+            tmp_node->haloid = thisnode->haloid;
+            tmp_node = tmp_node->BigChild;
+        }
+
+        nlines++;
     }
 
-    *startgroupnum = groupnum + 1;
 
     fclose(fp);
-    fprintf(stderr, "In load_found_progenitors:  Read in %" STR_FMT " lines\n", nlines);
+    if(nlines > 0)
+    {
+        *startgroupnum = groupnum + 1;
+        fprintf(stderr, "In load_found_progenitors:  Read in %" STR_FMT " lines\n", nlines);
+    }
+    else
+    {
+        min_snapshot = PARAMS.MIN_SNAPSHOT_NUM;
+        *startgroupnum = 0;
+        fprintf(stderr, "In load_found_progenitors:  Incomplete `%s` found - read in %" STR_FMT " lines. Recalculating the orphans ...\n", fname, nlines);
+    }
 
     return min_snapshot;
 }
@@ -220,8 +221,8 @@ int64 get_best_groupnum_wids(const id64 *sourceIds, const int64 Nids, struct gro
         // }
     }
 
-    my_free((void **)&DestRanks);
-    my_free((void **)&DestNcommon);
+    free(DestRanks);
+    free(DestNcommon);
 
     *rank = max_rank;
     if (max_rank > 0.0)
